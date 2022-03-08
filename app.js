e8657039b27776b335ec074d614fa2c501c3a4ba
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-03-08 14:09:57 -0300

    subo repositorio de prácticas de js

---
+++ app.js
@@ -0,0 +1,119 @@
+// const numero = prompt('ingrese un número')
+
+// if (numero === 100) {
+//   alert('Es igualx3 a 100')
+// } else{
+//   alert('no es igualx3 a 100')
+// }
+
+///////
+
+// const numero2 = parseInt(prompt('ingrese un número'))
+
+// if (numero2 === 100) {
+//   alert('Es igualx3 a 100')
+// } else{
+//   alert('no es igualx3 a 100')
+// }
+
+///////
+
+// const numero3 = parseInt(prompt('ingrese un número'))
+
+// if (numero3 === 100) {
+//   alert('Es 100')
+// } else if (numero3 === 50) {
+//   alert('es 50')
+// } else if (numero3 === 25) {
+//   alert('es 25')
+// } else{
+//   alert('Ingrese 100, 50 o 25')
+// }
+
+///////
+
+// const meses = parseInt(prompt('Ingrese un número del 1 al 12'))
+
+// switch (meses) {
+//   case 1:
+//     alert('Enero')
+//     break;
+
+//   case 2:
+//     alert('Febrero')
+//     break;
+
+//   case 3:
+//     alert('Marzo')
+//   break;
+
+//   case 4:
+//     alert('Abril')
+//     break;
+
+//   case 5:
+//     alert('Mayo')
+//     break;
+    
+//   case 6:
+//     alert('Junio')
+//   break;
+
+//   case 7:
+//     alert('Julio')
+//     break;
+
+//   case 8:
+//     alert('Agosto')
+//     break;
+
+//   case 9:
+//     alert('Septiembre')
+//   break;
+
+//   case 10:
+//     alert('Octubre')
+//     break;
+
+//   case 11:
+//     alert('Noviembre')
+//     break;
+    
+//   case 12:
+//     alert('Diciembre')
+//   break;
+
+//   default:
+//     alert('Ingrese un número correspondiente')
+//     break;
+// };
+
+///////
+
+// const booleano1 = false
+// const booleano2 = false
+
+// if (booleano1 || booleano2) {
+//   alert('TRUEx2')
+// } else {
+//   alert('FALSE something')
+// }
+
+// const booleano1 = false
+// const booleano2 = true
+
+// if (booleano1 && booleano2) {
+//   alert('TRUEx2')
+// } else {
+//   alert('FALSE something')
+// }
+
+///////
+const numero = 10
+const numero2 = 20
+
+if (numero === 10 && numero2 ===20) {
+  alert('Son iguales')
+} else{
+  alert('No son iguales')
+}
